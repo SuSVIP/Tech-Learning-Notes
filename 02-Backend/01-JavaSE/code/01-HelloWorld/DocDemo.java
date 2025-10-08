bea8279b2185c8f1557100d809708220fd89fb45
SuSVIP <2012573847@qq.com>
2025-10-08 11:08:32 +0800

    feat: 20251008-JavaSE-环境搭建与入门认知

---
+++ 02-Backend/01-JavaSE/code/01-HelloWorld/DocDemo.java
@@ -0,0 +1,19 @@
+package com.tarena.day01;
+
+/**
+ * 这是一个用于演示文档注释的类
+ * @author zzl
+ * @date 2025年10月08日 10:52
+ */
+public class DocDemo {
+
+    /**
+     * 计算两个整数的和
+     * @param a 第一个整数
+     * @param b 第二个整数
+     * @return 两个整数的和
+     */
+    public int add(int a,int b){
+        return a + b;
+    }
+}
---
+++ 02-Backend/01-JavaSE/code/01-HelloWorld/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.tarena.day01;
+
+/**
+ * @author zzl
+ * @date 2025年10月08日 10:51
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
